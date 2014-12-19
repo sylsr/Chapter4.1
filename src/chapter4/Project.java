8ae7057a297bc62b70cdf11f22103304585fb51d
Tyler Manning <sylsr9@gmail.com>
2014-12-18 23:23:24 -0700

    Chapter4-1
    Chapter 4

---
+++ src/chapter4/Project.java
@@ -0,0 +1,32 @@
+
+package chapter4;
+import java.util.Scanner;
+
+public class Project
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the year.");
+		int currentYear=scan.nextInt();
+		if(currentYear<1582)
+		{
+			System.out.println("Leap year did not exist, you suck.");
+		}
+		else if((currentYear%400)==0)
+		{
+			System.out.println("It is a leap year.");
+		}
+		else if(!((currentYear%4)==0||currentYear%100==0))
+		{
+			System.out.println("It is a leap year.");
+		}
+		else
+		{
+			System.out.println("it is not a leap year.");
+		}
+
+	}
+
+}
